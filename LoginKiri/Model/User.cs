2973e93e4bfd62afb99dcb0a054f6aacdb4d57c8
Crustbrns <georgejoof@gmail.com>
2022-06-04 21:36:43 +0300

    Login form added

---
+++ LoginKiri/Model/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginKiri
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
