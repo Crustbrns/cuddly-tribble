88243c471450a01c5482d9ba5534247709ab43c6
Crustbrns <georgejoof@gmail.com>
2022-06-11 20:09:33 +0300

    Added popups

---
+++ DishesStore/Db/Model/Dish.cs
@@ -0,0 +1,11 @@
+﻿namespace DishesStore.Db.Model
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ DishesStore/Db/Model/DishCategory.cs
@@ -0,0 +1,8 @@
+﻿namespace DishesStore.Data.Model
+{
+    public class DishCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
