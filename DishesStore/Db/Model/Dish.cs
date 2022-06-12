9462bfc70a7f92f6ef8e5ca7a2b23e66ec35ce79
Crustbrns <georgejoof@gmail.com>
2022-06-12 19:24:44 +0300

    Dish model updated & began using

--- DishesStore/Db/Model/Dish.cs
+++ DishesStore/Db/Model/Dish.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public int Price { get; set; }
+        public double Price { get; set; }
         public bool IsAvailable { get; set; }
     }
 }
