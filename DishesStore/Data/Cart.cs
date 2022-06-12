8884b9e550f5285719c512294197e8c2ecad27a9
Crustbrns <georgejoof@gmail.com>
2022-06-12 13:01:59 +0300

    Added cart & popup finally ended

---
+++ DishesStore/Data/Cart.cs
@@ -0,0 +1,12 @@
+﻿namespace DishesStore.Data
+{
+    public static class Cart
+    {
+        public static double Price { get; set; }
+        static Cart()
+        {
+            Price = 0;
+        }
+        public static void AddPrice(double price) => Price += price;
+    }
+}
