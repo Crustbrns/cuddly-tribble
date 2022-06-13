f0594194b606d673ed834c65a9d73dd17291df27
Crustbrns <georgejoof@gmail.com>
2022-06-13 09:16:09 +0300

    Added cart items' adding availabilty

--- DishesStore/Data/Cart.cs
+++ DishesStore/Data/Cart.cs
@@ -1,12 +1,17 @@
-﻿namespace DishesStore.Data
+﻿using DishesStore.Db.Model;
+
+namespace DishesStore.Data
 {
     public static class Cart
     {
         public static double Price { get; set; }
+        public static List<Dish> Dishes { get; set; }
         static Cart()
         {
             Price = 0;
+            Dishes = new List<Dish>();
         }
         public static void AddPrice(double price) => Price += price;
+        public static void AddItem(Dish dish) => Dishes.Add(dish);
     }
 }
