e4a7b459405d4560bc57474659412795fc8da0ff
Crustbrns <georgejoof@gmail.com>
2022-06-13 21:25:16 +0300

    Added online chat

---
+++ DishesStore/Data/Dialog.cs
@@ -0,0 +1,27 @@
+﻿using DishesStore.Db.Model;
+
+namespace DishesStore.Data
+{
+    public static class Dialog
+    {
+        public static List<Message> messages;
+        static Dialog()
+        {
+            messages = new List<Message>();
+        }
+
+        public static void AddMessage(Message message)
+        {
+            CheckLimit();
+            messages.Insert(0, message);
+        }
+
+        private static void CheckLimit()
+        {
+            if (messages.Count > 30)
+                messages.RemoveAt(messages.Count - 1);
+        }
+
+
+    }
+}
---
+++ DishesStore/Db/Model/Message.cs
@@ -0,0 +1,23 @@
+﻿namespace DishesStore.Db.Model
+{
+    public class Message
+    {
+        public DateTime Time { get; set; }
+        public string Body { get; set; }
+        public string UserName { get; set; }
+
+        public Message(string Body, string UserName)
+        {
+            this.Time = DateTime.Now;
+            this.Body = Body;
+            this.UserName = UserName;
+        }
+
+        public Message(string Body)
+        {
+            this.Time = DateTime.Now;
+            this.Body = Body;
+            this.UserName = "Anonymous";
+        }
+    }
+}
