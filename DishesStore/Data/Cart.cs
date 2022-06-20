315d212ad292fe019b0aee9d572ba7ea81fbadc2
Crustbrns <georgejoof@gmail.com>
2022-06-20 15:56:05 +0300

    Added pizzas' count values & updated functionality

--- DishesStore/Data/Cart.cs
+++ DishesStore/Data/Cart.cs
@@ -5,23 +5,23 @@ namespace DishesStore.Data
     public static class Cart
     {
         public static double Price { get; set; }
-        public static List<Dish> Dishes { get; set; }
+        public static List<DishToOrder> Dishes { get; set; }
         static Cart()
         {
             Price = 0;
-            Dishes = new List<Dish>();
+            Dishes = new List<DishToOrder>();
         }
         public static void AddPrice(double price) => Price += price;
-        public static void AddItem(Dish dish)
+        public static void AddItem(Dish dishToOrder, int countToOrder)
         {
-            if (Dishes.Count < 10) Dishes.Add(dish);
+            if (Dishes.Count < 10) Dishes.Add(new DishToOrder() { dish = dishToOrder, count = countToOrder });
         }
         public static double GetPrice()
         {
             double TotalPrice = 0;
-            Dishes.ForEach(x => TotalPrice += x.Price);
+            Dishes.ForEach(x => TotalPrice += x.dish.Price * x.count);
             return TotalPrice;
         }
-        public static void RemoveItem(Dish dish) => Dishes.Remove(dish);
+        public static void RemoveItem(DishToOrder dish) => Dishes.Remove(dish);
     }
 }
---
+++ DishesStore/Db/Model/DishToOrder.cs
@@ -0,0 +1,8 @@
+﻿namespace DishesStore.Db.Model
+{
+    public class DishToOrder
+    {
+        public Dish dish { get; set; }
+        public int count { get; set; }
+    }
+}
