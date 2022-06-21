089af759e37917164a07f7c028b9c70c702ecba4
Crustbrns <georgejoof@gmail.com>
2022-06-21 19:08:09 +0300

    Added Login Page & Changes in Toasts & AuthProps

---
+++ DishesStore/Db/Model/Auth/AuthProps.cs
@@ -0,0 +1,8 @@
+﻿namespace DishesStore.Db.Model.Auth
+{
+    public class AuthProps
+    {
+        public string? Login { get; set; } = null;
+        public string? Pass { get; set; } = null;
+    }
+}
---
+++ DishesStore/Db/Model/Auth/Registration/RegProps.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DishesStore.Db.Model.Auth.Registration
+{
+    public class RegProps
+    {
+        [Required]
+        [StringLength(8)]
+        public string Login { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Mail { get; set; }
+        [Required]
+        [StringLength(8)]
+        public string Pass { get; set; }
+
+        public RegProps()
+        {
+            Login = string.Empty;
+            Mail = string.Empty;
+            Pass = string.Empty;
+        }
+        public RegProps(string Login, string Mail, string Pass) : this()
+        {
+            this.Login = Login;
+            this.Mail = Mail;
+            this.Pass = Pass;
+        }
+        
+        public bool IsPassing()
+        {
+            return Login != null && Mail != null && Pass != null;
+        }
+    }
+}
