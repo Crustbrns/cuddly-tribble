9f26bd4f045fa4cfacbd533115ad478b67462df2
crustbrns <georgejoof@gmail.com>
2022-09-16 11:29:02 +0300

    Added two selects and options & calculation of them

--- JSburger/scripts/script.js
+++ JSburger/scripts/script.js
@@ -1,8 +1,12 @@
-function red(){
-    console.log('asd');
+function red() {
+    let first_sign = document.getElementById("select_st").selectedOptions[0].value;
+    let second_sign = document.getElementById("select_nd").selectedOptions[0].value;
+    let random = Math.random() * 100;
+    console.log(first_sign);
     // alert('asd');
+    document.getElementById("text-output").textContent = `Совместимость между ${first_sign} и ${second_sign} равна ${Math.floor(random)}%`;
 }
 
-document.getElementById("meowBtn").onclick = red();
+// document.getElementById("meowBtn").onclick = red();
 
 // $("meowBtn").bind("click", red());
