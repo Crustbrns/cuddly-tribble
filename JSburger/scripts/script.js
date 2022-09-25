a467de4b15f26564acc680eb1564d790e695eb05
crustbrns <georgejoof@gmail.com>
2022-09-25 22:07:40 +0300

    Minor additions in structure

--- JSburger/scripts/script.js
+++ JSburger/scripts/script.js
@@ -1,8 +1,25 @@
-let res = document.getElementsByClassName('chess-field');
+window.onload = () => {
+    createField();
+    paintField();
+};
 
-for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-        res[i * 8 + j].style.backgroundColor = i % 2 ?
-            (j % 2 ? '#F0DAB5' : '#B58763') : (j % 2 ? '#B58763' : '#F0DAB5');
+function paintField() {
+    let res = document.getElementsByClassName('chess-field');
+
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            res[i * 8 + j].style.backgroundColor = i % 2 ?
+                (j % 2 ? '#F0DAB5' : '#B58763') : (j % 2 ? '#B58763' : '#F0DAB5');
+        }
+    }
+}
+function createField() {
+    let field = document.getElementById('chess');
+
+    for (let i = 0; i < 64; i++) {
+        let tile = document.createElement("div");
+        tile.className = 'chess-field';
+        field.appendChild(tile);
+        console.log('asd');
     }
 }
