5956d70ba3e159b6f9230f70029fa73e49ca1644
crustbrns <georgejoof@gmail.com>
2022-11-04 17:08:07 +0200

    Added modules and media screen

--- Exhibitionkiri/scripts/scripts.js
+++ Exhibitionkiri/scripts/scripts.js
@@ -1,3 +1,9 @@
+const token = '25074899-aab4fea360b67611a921a5d7d';
+
+async function fetchRandomImage(){
+    fetch(`https://pixabay.com/api/?key=${token}&q=yellow+flowers&image_type=photo`)
+}
+
 window.onload = () => {
     let btn = document.getElementById('buzzbutton');
 }
