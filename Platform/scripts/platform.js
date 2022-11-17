961c1961e9bb332e21f6ba53f2047994553c381f
crustbrns <georgejoof@gmail.com>
2022-11-17 11:09:27 +0200

    Typescript initialize

---
+++ Platform/scripts/platform.js
@@ -0,0 +1,12 @@
+"use strict";
+class Platform {
+    constructor(x) {
+        this.x = x;
+    }
+}
+class Ball {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
---
+++ Platform/scripts/platform.ts
@@ -0,0 +1,15 @@
+class Platform {
+    public x: number;
+    constructor(x: number) {
+        this.x = x;
+    }
+}
+
+class Ball {
+    public x: number;
+    public y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
